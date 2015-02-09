0429ac2b88ed752ff18efb32389ce924b1dcfaf8
Golo Roden <webmaster@goloroden.de>
2015-02-09 20:45:56 +0100

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var tourism = require('tourism');
+
+module.exports = tourism({
+  analyse: {
+    server: [ '**/*.js', '!node_modules/**/*.js', '!coverage/**/*.js' ],
+    options: {
+      server: {
+        language: 'es5'
+      }
+    }
+  }
+});
---
+++ bin/forany.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var childProcess = require('child_process'),
+    fs = require('fs'),
+    path = require('path');
+
+var async = require('async'),
+    buntstift = require('buntstift'),
+    program = require('commander'),
+    updateNotifier = require('update-notifier');
+
+var packageJson = require('../package.json');
+
+var directories,
+    directoriesCount,
+    directoriesFailCount;
+
+updateNotifier({
+  packageName: packageJson.name,
+  packageVersion: packageJson.version
+}).notify();
+
+program
+  .version(packageJson.version)
+  .usage('[options] <command>')
+  .parse(process.argv);
+
+if (process.argv.length === 2) {
+  program.help();
+}
+
+directories = fs.readdirSync(process.cwd());
+
+directoriesCount = directories.length;
+directoriesFailCount = 0;
+
+async.each(directories, function (directory, callback) {
+  var directoryAbsolute = path.join(process.cwd(), directory);
+
+  if (!fs.statSync(directory).isDirectory()) {
+    directoriesCount--;
+    return callback(null);
+  }
+
+  childProcess.exec(program.args, {
+    cwd: directoryAbsolute
+  }, function (err, stdout, stderr) {
+    if (err) {
+      directoriesFailCount++;
+      buntstift.error('{{directory}} (exit code: {{code}})', { directory: directory, code: err.code });
+      buntstift.verbose(stderr);
+      return callback(null);
+    }
+
+    buntstift.success('{{directory}}', { directory: directory });
+    buntstift.verbose(stdout);
+    callback(null);
+  });
+}, function () {
+  if (directoriesFailCount > 0) {
+    return buntstift.warn('Processing {{count}} of {{total}} directories failed.', { count: directoriesFailCount, total: directoriesCount });
+  }
+
+  buntstift.info('Successfully processed {{count}} directories.', { count: directoriesCount });
+});
