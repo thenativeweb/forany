41379d5d667eacff7e2fcf4cc8acb4b4cbe973bd
Golo Roden <webmaster@goloroden.de>
2015-09-15 12:06:29 +0200

    Improved output.

--- bin/forany.js
+++ bin/forany.js
@@ -17,18 +17,22 @@ var directories,
     directoriesCount,
     directoriesFailCount;
 
+var isVerbose = function () {
+  return (process.argv.indexOf('--verbose') !== -1) || (process.argv.indexOf('-v') !== -1);
+};
+
 updateNotifier({
   packageName: packageJson.name,
   packageVersion: packageJson.version
 }).notify();
 
-program
-  .version(packageJson.version)
-  .usage('[options] <command>')
-  .option('--color', 'enforce color output')
-  .option('--no-color', 'disable color output')
-  .option('-v, --verbose', 'verbose output')
-  .parse(process.argv);
+program.
+  version(packageJson.version).
+  usage('[options] <command>').
+  option('--color', 'enforce color output').
+  option('--no-color', 'disable color output').
+  option('-v, --verbose', 'verbose output').
+  parse(process.argv);
 
 if (process.argv.length === 2) {
   program.help();
@@ -47,6 +51,11 @@ async.eachSeries(directories, function (directory, callback) {
     return callback(null);
   }
 
+  if (isVerbose()) {
+    buntstift.info('Processing {{directory}}...', { directory: directory });
+    buntstift.newLine();
+  }
+
   buntstift.waitFor(function (stopWaiting) {
     childProcess.exec(program.args, {
       cwd: directoryAbsolute
@@ -55,20 +64,28 @@ async.eachSeries(directories, function (directory, callback) {
 
       if (err) {
         directoriesFailCount++;
+        buntstift.verbose(stderr.replace(/\n/g, '\n  '));
         buntstift.error('{{directory}} (exit code: {{code}})', { directory: directory, code: err.code });
-        buntstift.verbose(stderr);
         return callback(null);
       }
 
+      buntstift.verbose(stdout.replace(/\n/g, '\n  '));
       buntstift.success('{{directory}}', { directory: directory });
-      buntstift.verbose(stdout);
+
+      if (isVerbose()) {
+        buntstift.line();
+      }
+
       callback(null);
     });
   });
 }, function () {
+  if (!isVerbose()) {
+    buntstift.line();
+  }
+
   if (directoriesFailCount > 0) {
     return buntstift.warn('Processing {{count}} of {{total}} directories failed.', { count: directoriesFailCount, total: directoriesCount });
   }
-
   buntstift.success('Successfully processed {{count}} directories.', { count: directoriesCount });
 });
